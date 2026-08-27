dda434fb54cd620a4af2ce92bfa383f6fd4953cd
Alok Paul <alokpaul369.com>
2026-08-27 20:27:43 +0600

    Express server and MongoDB connection

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "MERN Backend API is running"
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
