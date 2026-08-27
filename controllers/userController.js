dcc7a5cec712bb93f603599fcb2f6ce75d249e5f
Alok Paul <alokpaul369.com>
2026-08-28 04:30:11 +0600

    User Management system

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,7 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+//Get Profile...........................
 const getProfile = async (req, res) => {
   try {
     res.status(200).json({
@@ -12,4 +16,109 @@ const getProfile = async (req, res) => {
   }
 };
 
-module.exports = {getProfile};
+//Update Profile...........................
+const updateProfile = async (req, res) => {
+  try {
+    const {name, email} = req.body;
+    const user = await User.findById(req.user._id);
+    if(!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+    if(name) {
+      user.name = name;
+    }
+    if(email) {
+      user.email = email;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      message: "Profile updated successfully",
+      user: {
+        id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        role: updatedUser.role
+      }
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({
+        message: "Email already exists"
+      });
+    }
+    res.status(500).jason({
+      message: "Server error",
+      error: error.message
+    });
+  }
+};
+
+// Change Password..........................
+const changePassword = async (req, res) => {
+  try {
+    const {currentPassword, newPassword} = req.body;
+    if (!currentPassword || !newPassword) {
+      return res.status(400).json({
+        message: "Please provide current password and new password"
+      });
+    }
+    if(newPassword.length < 8) {
+      return res.status(400).json({
+        message: "New password must be at least 8 characters"
+      });
+    }
+     const user = await User.findById(req.user._id);
+    if(!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      currentPassword, user.password
+    );
+    if(!isPasswordCorrect) {
+      return res.status(401).json({
+        message: "Current password is incorrect"
+      });
+    }
+    user.password = await bcrypt.hash(newPassword,16);
+    await user.save();
+    res.status(200).json({
+      message: "Password changed successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message
+    });
+  }
+};
+
+// Delete Account..................................
+const deleteAccount = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    if(!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+    await user.deleteOne();
+    res.status(200).json({
+      message: "Account deleted successfully"
+    });
+  } catch(error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message
+    });
+  }
+};
+module.exports = {
+  getProfile,
+  updateProfile,
+  changePassword,
+  deleteAccount
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,25 @@
 const express = require("express");
-const {getProfile} = require("../controllers/userController");
+const {
+  getProfile,
+  updateProfile,
+  changePassword,
+  deleteAccount
+} = require("../controllers/userController");
+
 const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
-router.get("/profile", protect, getProfile)
+
+// Get Profile
+router.get("/profile", protect, getProfile);
+
+// Update Profile
+router.put("/profile", protect, updateProfile);
+
+// Change Password
+router.put("/change-password", protect, changePassword);
+
+// Delete Account
+router.delete("/profile", protect, deleteAccount);
 
 module.exports = router;
